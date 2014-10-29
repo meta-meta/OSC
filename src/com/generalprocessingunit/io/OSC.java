14cee6ce39abb71debd77054b2f9e25f7caff807
Paul M. Christian <paulmchristian@gmail.com>
2014-10-29 18:43:00 -0500

    add OSC in; add multiple message sending

--- src/com/generalprocessingunit/io/OSC.java
+++ src/com/generalprocessingunit/io/OSC.java
@@ -1,13 +1,17 @@
 package com.generalprocessingunit.io;
 
+import com.illposed.osc.OSCListener;
 import com.illposed.osc.OSCMessage;
+import com.illposed.osc.OSCPortIn;
 import com.illposed.osc.OSCPortOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class OSC {
     private static OSCPortOut oscPortOut;
+    private static OSCPortIn oscPortIn;
 
     protected OSC() { /*Singleton*/ }
 
@@ -22,8 +26,28 @@ public class OSC {
         return oscPortOut;
     }
 
+    private static OSCPortIn getOscPortIn() {
+        if(null == oscPortIn) {
+            try {
+                oscPortIn = new OSCPortIn(7400);
+                oscPortIn.startListening();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        return oscPortIn;
+    }
+
     public static void sendMsg(String address, Object message) {
         sendMsg(address, Arrays.asList((Object) message));
+    }
+
+    public static void sendMsg(String address, Object ... messages) {
+        List<Object> msgList = new ArrayList<>(messages.length);
+        for(Object message : messages) {
+            msgList.add(message);
+        }
+        sendMsg(address, msgList);
     }
 
     public static void sendMsg(String address, List<Object> messages) {
@@ -33,5 +57,9 @@ public class OSC {
         } catch (Exception e) {
             System.out.println("Couldn't send OSC");
         }
+    }
+
+    public static void addListener(String address, OSCListener listener) {
+        getOscPortIn().addListener(address, listener);
     }
 }
